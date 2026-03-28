6df49d0c4e3c46bc462719a9040b429508d80fbd
rtarn <rtarnowski123@gmail.com>
2026-03-28 12:16:33 +0100

    Change database from H2 to postgres

---
+++ src/test/java/com/example/couponapp/controller/BaseITTest.java
@@ -0,0 +1,48 @@
+package com.example.couponapp.controller;
+
+import com.example.couponapp.repository.CouponRepository;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+public class BaseITTest {
+
+    @Autowired
+    CouponRepository couponRepository;
+
+    static PostgreSQLContainer postgres = new PostgreSQLContainer(DockerImageName.parse("postgres:16-alpine"))
+            .withDatabaseName("coupon_app")
+            .withUsername("test")
+            .withPassword("test");
+
+    @BeforeAll
+    static void beforeAll() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgres.stop();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.flyway.url", postgres::getJdbcUrl);
+        registry.add("spring.flyway.user", postgres::getUsername);
+        registry.add("spring.flyway.password", postgres::getPassword);
+    }
+
+    @BeforeEach
+    void setUp() {
+        couponRepository.deleteAll();
+    }
+
+}
